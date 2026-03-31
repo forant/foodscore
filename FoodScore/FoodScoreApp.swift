e5eb7c4465ab3fc9caa4c4ce2e4a07937e95daca
Tim Foran <tforan@gmail.com>
2026-03-31 15:47:13 -0700

    Initial Commit

---
+++ FoodScore/FoodScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodScoreApp.swift
+//  FoodScore
+//
+//  Created by Timothy Foran on 3/31/26.
+//
+
+import SwiftUI
+
+@main
+struct FoodScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
